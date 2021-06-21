fef2478ad3b638d0e99fe1ae505e67565fcdee2a
Kaiser Samrat <kaisersamrat136@gmail.com>
2021-06-21 18:21:31 +0600

    solved

---
+++ left-rotation.py
@@ -0,0 +1,22 @@
+def rotateLeft(a, d,n):
+    
+    length = len(a)     
+    newArray = [0 for x in a] # Initialize 0 in an array of size 5.
+    for i in range(n):
+        o = i-d          
+        newArray[o]=str(a[i]) 
+    result = " ".join(newArray)
+    return result
+
+
+def main():
+    
+    arr = [1,2,3,4,5]
+    d = 4
+    n = 5
+    result = rotateLeft(arr, d,n)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
